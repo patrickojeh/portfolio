db584537b64a3dc1c1dc3da510740d704ae1e4df
Patrick Ojeh <patrickojeh@gmail.com>
2021-11-28 16:10:10 +0100

    added 404

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,10 +1,12 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   return <header>
     <div className="mw9 ph5-l ph4-ns ph35 center pt4">
       <div className="flex items-center-ns justify-between-ns flex-row-ns flex-column">
-        <span className="logo">Patrick O.</span>
+        <span className="logo">
+          <Link to="/">Patrick O.</Link>
+        </span>
         <ol className="list pa0 ma0 mt0-ns mt4">
           <li className="fl w-auto pl4-ns">
             <NavLink to="/about">About</NavLink>
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,16 @@
-import { Fragment } from "react";
+import Layout from "../components/Layout/Layout";
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
-  return <Fragment>
-    <h1>NotFound</h1>
-  </Fragment>
+  return <Layout>
+    <div className="mw7 ph5-l ph4-ns ph35 center pt6-l pt5 pb6-l pb4">
+      <h1 className="mw8 f2-l f3-m f4 lh-copy pr6-l mb3-l mb4">
+        404 : Page Not Found
+      </h1>
+      <p className="f4-ns lh-copy mb5-l">The page you tried to access cannot be found. Maybe try going home or contact me instead.</p>
+      <Link to="/contact" className="f4-ns bb pb1 b--white-20">Contact me</Link>
+    </div>
+  </Layout>
 }
 
 export default NotFound;
