a534474d68bb3abe7f593474f5933e49b77b83b1
Patrick Ojeh <patrickojeh@gmail.com>
2022-01-06 15:36:07 +0100

    changed cta from Contact me to About me

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
     <div className="mw9 ph5-l ph4-ns ph35 center pt6-l pt5">
       <img src={signature} alt="My signature in svg" className="h-auto-ns h25 mb3" />
       <h1 className="mw8 f2-l f3-m f4 lh-copy pr6-l mb5-l mb4">Hi, I’m Patrick Ojeh. I’m a product designer &amp; part-time developer currently based in Lagos Nigeria. I lead product design at Mobnia where i design & code to solve business problems. I have experience in visual design, prototyping, CSS architecture &amp; React.</h1>
-      <Link to="/contact" className="f4-ns bb pb1 b--white-20">Contact me</Link>
+      <Link to="/about" className="f4-ns bb pb1 b--white-20">About me</Link>
       <div className="project-grid mt6-l mt5">
           <div className="project-grid__item">
             <div className="bg-near-black h5-l h4-ns h5 br2 mb4-ns mb3">
@@ -55,7 +55,7 @@ const Home = () => {
             <p className="f4-ns lh-copy mt1">Side Project</p>
           </div>
           <div className="project-grid__item">
-            <div className="bg-near-black h5-l h4-ns h5 br2 mb4-ns mb3">              
+            <div className="bg-near-black h5-l h4-ns h5 br2 mb4-ns mb3">
               <a href="https://ussd.patrickojeh.com" target="_blank">
                 <img src={ussdCoverImage} alt="Project cover" className="h-100 w-100" />
               </a>
