256e54dd5bc652dad2b6f26122e69418a0ba08c5
Patrick Ojeh <patrickojeh@gmail.com>
2022-05-09 11:54:06 +0100

    modify 404

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -7,8 +7,8 @@ const NotFound = () => {
       <h1 className="mw8 f2-l f3-m f4 lh-copy pr6-l mb3-l mb4">
         404 : Page Not Found
       </h1>
-      <p className="f4-ns lh-copy mb5-l">The page you tried to access cannot be found. You should try going home or contact me instead.</p>
-      <Link to="/contact" className="f4-ns bb pb1 b--white-20">Contact me</Link>
+      <p className="lh-copy mb5-l">The page you tried to access cannot be found. You should try going home or contact me instead.</p>
+      <Link to="/contact" className="bb pb1 b--white-20">Contact me</Link>
     </div>
   </Layout>
 }
