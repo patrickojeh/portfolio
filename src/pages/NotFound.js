ee7996a1924c07e02e49ddf822c4de8b63614e6f
Patrick Ojeh <patrickojeh@gmail.com>
2022-05-09 11:55:31 +0100

    modify 404

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 const NotFound = () => {
   return <Layout>
     <div className="mw7 ph5-l ph4-ns ph35 center pt6-l pt5 pb5-l pb4">
-      <h1 className="mw8 f2-l f3-m f4 lh-copy pr6-l mb3-l mb4">
+      <h1 className="mw8 pr6-l mb3-l mb4">
         404 : Page Not Found
       </h1>
-      <p className="lh-copy mb5-l">The page you tried to access cannot be found. You should try going home or contact me instead.</p>
+      <p className="mb5-l">The page you tried to access cannot be found. You should try going home or contact me instead.</p>
       <Link to="/contact" className="bb pb1 b--white-20">Contact me</Link>
     </div>
   </Layout>
